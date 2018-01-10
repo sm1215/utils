b403de15ee298ba603e237bc76327190aa746d76
Sam <smiller@createareaction.com>
2018-01-10 12:50:46 -0500

    introduced validationType weighting

--- validator/js/validator.js
+++ validator/js/validator.js
@@ -1,22 +1,26 @@
 /*
 *  Small frontend validator module.
 *  Author: Sam Miller
-*  Version 0.0.1
+*  Version 0.0.2
 *  [todo]:
-*    1) build other todo comments throughout code
-*    2) add constructor or initialization function
-*      2b) accept user arguments on constructor for customization
+*    1) add constructor or initialization function
+*      1b) accept user arguments on constructor for customization
 *          some initial arguments that should be available:
 *            + specify errorWriteTarget
 *            + specify where error messages appear (under field or stacked in a single element)
 *            + extra validationTypes could be introduced by outside dev
-*    3) allow more than one test to fail per field. right now, one is enough, but this could be useful for debugging purposes while developing
+*    2) allow more than one test to fail per field. right now, one is enough, but this could be useful for debugging purposes while developing
 */
+
+//[Note]: It might be useful to weight certain validationTypes higher than others.
+//The 'required' test should be performed before any format-specific tests.
+//i.e. it doesn't make sense to tell a user their email format is incorrect if the field is blank
 
 var validator = (function(){
   var formElements = ['input', 'textarea', 'select']; //nodeNames (console lists these in caps)
   var validationTypes = {
     required: {
+      weight: 0,
       test: function(value){
         if(!value || !value.length){
           return false;
@@ -26,24 +30,28 @@ var validator = (function(){
       error: 'This field is required.'
     },
     email: {
+      weight: 10,
       test: function(value){
         return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
       },
       error: 'Incorrect email format.'
     },
     phone: {
+      weight: 10,
       test: function(value){
         return /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(value);
       },
       error: 'Incorrect phone format.'
     },
     date: {
+      weight: 10,
       test: function(value){
         return /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(([\d]{4})|([\d]{2}))$/.test(value);
       },
       error: 'Incorrect date format.'
     },
     number: {
+      weight: 10,
       test: function(value){
         if(!value || value.length <= 0){
           return false;
@@ -53,6 +61,7 @@ var validator = (function(){
       error: 'This field only accepts numbers.'
     },
     letters: {
+      weight: 10,
       test: function(value){
         if(!value || value.length <= 0){
           return false;
@@ -68,7 +77,8 @@ var validator = (function(){
   var errorWriteTarget = document.querySelector('#errors');
 
   var _validate = function(form){
-    var fields = [], failedFields = [];
+    var fields = [];
+    var failedFields = [];
 
     fields = _findFields(form);
      _unmarkFields(fields);
@@ -105,22 +115,18 @@ var validator = (function(){
       }
 
       if(vtypes){
-        if(required != null && _arrayContainsString(vtypes, 'required') !== false){
+        if(required != null && _arrayContainsString(vtypes, 'required') === false){
           vtypes.push('required');
         }
 
-        //[todo]: it might be useful to weight certain vtypes higher than others.
-        //the 'required' test should be performed before any format-specific tests.
-        //i.e. it doesn't make sense to tell a user their email format is incorrect if the field is blank
+        _sortVtypesByWeight(vtypes);
+
         for (var j = 0; j < vtypes.length; j++) {
           var vt = vtypes[j];
           var test = validationTypes[vt].test(f.value);
 
-          console.log('vt', vt, 'test', test, 'value', f.value);
-
           if(test == false){
             valid = false;
-
             if(_arrayContainsNode(failedFields, f) === false){
               failedFields.push({ el: f, vtype: vt, error: validationTypes[vt].error });
               break;
@@ -130,6 +136,36 @@ var validator = (function(){
       }
     }
     return failedFields;
+  }
+
+  //This will take the vtypes provided by the dev in html and sort them according to the weights found in the validationTypes object
+  //Returns the sorted array
+  var _sortVtypesByWeight = function(vtypes){
+    if(vtypes.length <= 1){
+      return;
+    }
+
+    var vt = validationTypes;
+    var sorted = sort(vtypes);
+
+    function sort(arr, i){
+      i == undefined ? i = 0 : i;
+      if(i == arr.length){
+        return arr;
+      }
+
+      for (var j = i; j < arr.length - 1; j++) {
+        var keyA = arr[j];
+        var keyB = arr[j + 1];
+        if(vt[keyA].weight > vt[keyB].weight){
+          var swap;
+          swap = arr[j + 1];
+          arr[j + 1] = arr[j];
+          arr[j] = swap;
+        }
+      }
+      return sort(arr, i+1);
+    }
   }
 
   var _unmarkFields = function(fields){
