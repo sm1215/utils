869274b23b10bf8c6b682c587276ab96efb88be0
sm1215 <sm1215@gmail.com>
2018-04-02 10:29:32 -0400

    setup for singleton work

--- validator/src/js/index.js
+++ validator/src/js/index.js
@@ -1,27 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const Validator = require('./validator.js');
-  var validator = new Validator({
-    form: 'form',
-    inputSelector: 'data-vtypes',
-    errorTarget: 'inline',
-    validationTypes: [
-      {
-        name: 'price',
-        weight: 20,
-        test: function(value){
-          return /(\d)+\.(\d){0,2}$/.test(value);
-        },
-        error: 'Incorrect price format.'
-      }
-    ]
-  });
+  // ES6 Module version
+  // const Validator = require('./validator.js');
+  // var validator = new Validator({
+  //   form: 'form',
+  //   inputSelector: 'data-vtypes',
+  //   errorTarget: 'inline',
+  //   validationTypes: [
+  //     {
+  //       name: 'price',
+  //       weight: 20,
+  //       test: function(value){
+  //         return /(\d)+\.(\d){0,2}$/.test(value);
+  //       },
+  //       error: 'Incorrect price format.'
+  //     }
+  //   ]
+  // });
 
-  document.querySelector('input[type="submit"]').addEventListener('click', function(e){
-    e.preventDefault();
-    validator.validate(document.querySelector('form'));
-  });
+  // document.querySelector('input[type="submit"]').addEventListener('click', function(e){
+  //   e.preventDefault();
+  //   validator.validate(document.querySelector('form'));
+  // });
   //run on refresh
   // validator.validate(document.querySelector('form'));
 
   // console.log('errors: ', validator.getErrorString());
+
+
+  // Singleton version
+  var validator = Validator;
+  validator.construct({
+    form: document.querySelector('form')
+  });
+  validator.validate();
 });
